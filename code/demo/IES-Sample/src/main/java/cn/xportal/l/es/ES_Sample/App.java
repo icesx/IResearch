5665fd2315db1f32f7b371b1126c70c908df24dd
icesx <icesxrun@gmail.com>
2018-03-20 19:10:48 +0800

    Ies-sample

---
+++ code/demo/IES-Sample/src/main/java/cn/xportal/l/es/ES_Sample/App.java
@@ -0,0 +1,13 @@
+package cn.xportal.l.es.ES_Sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
