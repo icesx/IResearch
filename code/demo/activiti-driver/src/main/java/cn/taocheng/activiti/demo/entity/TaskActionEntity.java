c657f4cef921f009fe1a42903fa93fc1a97256a3
chengtao <chengtao@ithink>
2020-05-19 15:40:48 +0800

    rename project

--- code/demo/activiti-driver/src/main/java/cn/taocheng/activiti/demo/entity/TaskActionEntity.java
+++ code/demo/activiti-driver/src/main/java/cn/taocheng/activiti/demo/entity/TaskActionEntity.java
@@ -14,7 +14,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "task_action")
+@Table(name = "driver_task_action")
 public class TaskActionEntity implements Serializable {
 
 	/**
