02c12ada84704b12d0d0b96cfe3e76b5d220d6d4
chengtao <chengtao@ithink>
2020-05-21 11:39:18 +0800

    core junit with diff yml

--- code/demo/activiti-driver/activit-driver-core/src/test/java/cn/taocheng/activiti/driver/core/manager/TestActivitiManager.java
+++ code/demo/activiti-driver/activit-driver-core/src/test/java/cn/taocheng/activiti/driver/core/manager/TestActivitiManager.java
@@ -15,18 +15,37 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import cn.taocheng.activiti.driver.core.bean.Assginee;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Configuration
+@SpringBootApplication
+@EnableAutoConfiguration
+@ActiveProfiles("activit-driver-core") 
+@EntityScan("cn.taocheng.activiti.driver.core.entity")
+@EnableJpaRepositories("cn.taocheng.activiti.driver.core.dao")
+@ComponentScan(basePackages = { "cn.taocheng.activiti.driver.core.**", "cn.taocheng.activiti.driver.sample" })
 public class TestActivitiManager {
 	private static final Logger logger = LoggerFactory.getLogger(TestActivitiManager.class);
 
 	@Autowired
 	private IActivitiManager activitiManager;
+
+	@Test
+	public void deploy() {
+		activitiManager.deployProcess("processes/pig.bpmn");
+	}
 
 	@Test
 	public void getProcess() {
@@ -62,6 +81,7 @@ public class TestActivitiManager {
 	public void listTaskAction() {
 		activitiManager.listTaskAction().stream().forEach(x -> logger.info(x.toString()));
 	}
+
 	@Test
 	public void deleteProcess() {
 		activitiManager.deleteProcessInstance("115005", "test");
